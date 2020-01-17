e1548b12b294c55b6b79422250b4f3bd2ac313b7
AlexanderDLe <alexanderledev@gmail.com>
2020-01-17 15:09:47 -0800

    337. House Robber 3

--- build/DP/HouseRobber3.js
+++ build/DP/HouseRobber3.js
@@ -1,8 +1,38 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * 337. House Robber 3
  */
-Object.defineProperty(exports, "__esModule", { value: true });
+const TreeClass_1 = require("../DataStructures/TreeClass");
+const rob = (root) => {
+    if (!root)
+        return 0;
+    let val = 0;
+    if (root.left)
+        val += rob(root.left.left) + rob(root.left.right);
+    if (root.right)
+        val += rob(root.right.left) + rob(root.right.right);
+    return Math.max(root.val + val, rob(root.left) + rob(root.right));
+};
+const robB = (root) => {
+    const helper = (node) => {
+        if (!node)
+            return [0, 0];
+        const [lr, ln] = helper(node.left);
+        const [rr, rn] = helper(node.right);
+        return [
+            node.val + ln + rn,
+            Math.max(lr + rr, ln + rn, lr + rn, ln + rr)
+        ];
+    };
+    return Math.max(...helper(root));
+};
 exports.default = () => {
-    console.log('house rob 3');
+    const t = new TreeClass_1.TreeNode(3);
+    t.left = new TreeClass_1.TreeNode(2);
+    t.right = new TreeClass_1.TreeNode(3);
+    t.left.right = new TreeClass_1.TreeNode(3);
+    t.right.right = new TreeClass_1.TreeNode(1);
+    console.log(robB(t));
+    TreeClass_1.BinaryPreorderTraversal(t);
 };
--- src/DP/HouseRobber3.ts
+++ src/DP/HouseRobber3.ts
@@ -1,7 +1,39 @@
 /**
  * 337. House Robber 3
  */
+import { TreeNode, BinaryPreorderTraversal } from '../DataStructures/TreeClass';
+
+type Node = TreeNode<number> | null;
+
+const rob = (root: Node): number => {
+    if (!root) return 0;
+
+    let val = 0;
+    if (root.left) val += rob(root.left.left) + rob(root.left.right);
+    if (root.right) val += rob(root.right.left) + rob(root.right.right);
+
+    return Math.max(root.val + val, rob(root.left) + rob(root.right));
+};
+
+const robB = (root: Node): number => {
+    const helper = (node: Node): number[] => {
+        if (!node) return [0, 0];
+        const [lr, ln] = helper(node.left);
+        const [rr, rn] = helper(node.right);
+        return [
+            node.val + ln + rn,
+            Math.max(lr + rr, ln + rn, lr + rn, ln + rr)
+        ];
+    };
+    return Math.max(...helper(root));
+};
 
 export default () => {
-    console.log('house rob 3');
+    const t = new TreeNode(3);
+    t.left = new TreeNode(2);
+    t.right = new TreeNode(3);
+    t.left.right = new TreeNode(3);
+    t.right.right = new TreeNode(1);
+    console.log(robB(t));
+    BinaryPreorderTraversal(t);
 };
