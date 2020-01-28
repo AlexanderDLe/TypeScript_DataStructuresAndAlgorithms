b69203178abe9d9c793caa6a97df798724dae7b2
alexanderdle <alexanderledev@gmail.com>
2020-01-28 14:41:03 -0800

    279. Perfect Squares

--- build/DP/PerfectSquares.js
+++ build/DP/PerfectSquares.js
@@ -13,18 +13,16 @@ const numSquares = (n) => {
         else
             break;
     }
-    const DP = new Array(n + 1).fill(0);
+    const DP = new Array(n + 1).fill(n + 1);
     for (let i = 1; i <= n; i++) {
         if (squares.includes(i))
             DP[i] = 1;
         else {
-            let min = n + 1;
             for (let square of squares) {
                 if (square > i)
                     break;
-                min = Math.min(min, DP[i - square] + 1);
+                DP[i] = Math.min(DP[i], DP[i - square] + 1);
             }
-            DP[i] = min;
         }
     }
     return DP[n];
--- src/DP/PerfectSquares.ts
+++ src/DP/PerfectSquares.ts
@@ -11,16 +11,14 @@ const numSquares = (n: number): number => {
         else break;
     }
 
-    const DP = new Array(n + 1).fill(0);
+    const DP = new Array(n + 1).fill(n + 1);
     for (let i = 1; i <= n; i++) {
         if (squares.includes(i)) DP[i] = 1;
         else {
-            let min = n + 1;
             for (let square of squares) {
                 if (square > i) break;
-                min = Math.min(min, DP[i - square] + 1);
+                DP[i] = Math.min(DP[i], DP[i - square] + 1);
             }
-            DP[i] = min;
         }
     }
     return DP[n];
