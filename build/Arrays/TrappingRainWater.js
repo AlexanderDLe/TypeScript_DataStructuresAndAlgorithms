76d005ae8604afa0b761facd76f0f8458932ac94
alexanderdle <alexanderledev@gmail.com>
2020-03-21 16:04:14 -0700

    BurstballonsB

---
+++ build/Arrays/TrappingRainWater.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ *  42. Trapping Rain Water
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const ;
+() => {
+};
+exports.default = () => {
+};
--- src/DP/BurstBalloons.ts
+++ src/DP/BurstBalloons.ts
@@ -9,9 +9,9 @@ const maxCoins = (nums: number[]): number => {
     const n = nums.length;
     const dp = [...Array(n + 2)].map(() => Array(n + 2).fill(0));
 
-    for (let len = 1; len <= n; len++) {
-        for (let i = 1; i + len <= n + 1; i++) {
-            const j = i + len - 1;
+    for (let window = 1; window <= n; window++) {
+        for (let i = 1; i + window <= n + 1; i++) {
+            const j = i + window - 1;
             for (let k = i; k <= j; k++) {
                 dp[i][j] = Math.max(
                     dp[i][j],
@@ -19,15 +19,35 @@ const maxCoins = (nums: number[]): number => {
                         vals[i - 1] * vals[k] * vals[j + 1] +
                         dp[k + 1][j]
                 );
-                console.log(`len: ${len} | i: ${i} | j: ${j} | k: ${k}`);
+                console.log(`window: ${window} | i: ${i} | j: ${j} | k: ${k}`);
                 console.log(dp);
             }
         }
     }
     return dp[1][n];
 };
+const maxCoinsB = (nums: number[]): number => {
+    let vals = [1, ...nums, 1];
+    let n = nums.length;
+    let dp = [...Array(n + 2)].map(() => Array(n + 2).fill(0));
 
+    for (let window = 1; window <= n; window++) {
+        for (let left = 1; left + window <= n + 1; left++) {
+            const right = left + window - 1;
+            for (let burst = left; burst <= right; burst++) {
+                dp[left][right] = Math.max(
+                    dp[left][right],
+                    dp[left][burst - 1] +
+                        vals[left - 1] * vals[burst] * vals[right + 1] +
+                        dp[burst + 1][right]
+                );
+            }
+        }
+    }
+
+    return dp[1][n];
+};
 export default () => {
     const nums = [3, 1, 5, 8];
-    console.log(maxCoins(nums));
+    console.log(maxCoinsB(nums));
 };
