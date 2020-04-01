89848f76c89aa457e66180e1aa3ee43156f71280
alexanderdle <alexanderledev@gmail.com>
2020-03-31 17:39:48 -0700

    Setup Excel

---
+++ build/Math/ExcelSheetColumnNumber.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ *  171. Excel Sheet Column Number
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const titleToNumber = (s) => {
+    return 0;
+};
+exports.default = () => {
+    console.log('excel');
+};
---
+++ src/Math/ExcelSheetColumnNumber.ts
@@ -0,0 +1,11 @@
+/**
+ *  171. Excel Sheet Column Number
+ */
+
+const titleToNumber = (s: string): number => {
+    return 0;
+};
+
+export default () => {
+    console.log('excel');
+};
